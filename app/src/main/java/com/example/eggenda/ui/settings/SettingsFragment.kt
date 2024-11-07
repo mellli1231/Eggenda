1d0b6e2860598b29eb2a610aefb372893f33c655
Melody <153961300+mellli1231@users.noreply.github.com>
2024-11-07 15:42:21 -0800

    implement terms and conditions dialog fragment

--- app/src/main/java/com/example/eggenda/ui/settings/SettingsFragment.kt
+++ app/src/main/java/com/example/eggenda/ui/settings/SettingsFragment.kt
@@ -1,11 +1,18 @@
 package com.example.eggenda.ui.settings
 
 
+import android.app.AlertDialog
+import android.app.Dialog
+import android.content.DialogInterface
 import android.content.Intent
 import android.content.SharedPreferences
 import android.content.SharedPreferences.Editor
 import android.os.Bundle
+import android.view.View
+import android.widget.EditText
+import android.widget.RadioGroup
 import android.widget.Toast
+import androidx.fragment.app.DialogFragment
 import androidx.preference.Preference
 import androidx.preference.PreferenceFragmentCompat
 import androidx.preference.PreferenceManager
@@ -13,7 +20,10 @@ import com.example.eggenda.LocaleHelper
 import com.example.eggenda.R
 
 class SettingsFragment : PreferenceFragmentCompat(), SharedPreferences.OnSharedPreferenceChangeListener{
-
+    companion object {
+        const val DIALOG_KEY = "dialog"
+        const val TERMS_CONDITIONS_DIALOG = 0
+    }
     private lateinit var sharedPreferences: SharedPreferences
     private lateinit var editor: Editor
 
@@ -51,6 +61,57 @@ class SettingsFragment : PreferenceFragmentCompat(), SharedPreferences.OnSharedP
             }
             true
         }
+
+        val termsConditions: Preference? = findPreference("terms")
+        termsConditions?.setOnPreferenceClickListener {
+            println("terms and conditions clicked")
+            showMyDialogFragment(TERMS_CONDITIONS_DIALOG, R.string.conditions_header.toString())
+
+            true
+        }
+    }
+
+    class MyRunsDialogFragment : DialogFragment(), DialogInterface.OnClickListener {
+        override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
+            //get arguments and dialog details
+            lateinit var ret: Dialog
+            val bundle = arguments
+            val dialogId = bundle?.getInt(DIALOG_KEY)
+            val builder = AlertDialog.Builder(requireActivity())
+            val title = bundle?.getString("TITLE") ?: ""
+
+            if (dialogId == TERMS_CONDITIONS_DIALOG) {
+                val view: View = requireActivity().layoutInflater.inflate(
+                    R.layout.dialog_terms_conditions,
+                    null
+                )
+                builder.setView(view)
+                builder.setTitle(R.string.conditions_header)
+
+                builder.setNegativeButton(R.string.cancel_button, this)
+                ret = builder.create()
+            }
+
+            return ret
+        }
+
+        //function when buttons are clicked
+        override fun onClick(dialog: DialogInterface, item: Int) {
+            if(item == DialogInterface.BUTTON_NEGATIVE) {
+                println("fragment negative button clicked")
+            }
+        }
+    }
+
+    //show dialog fragment
+    private fun showMyDialogFragment(dialogType: Int, title: String) {
+        val myDialog = MyRunsDialogFragment()
+        val bundle = Bundle().apply {
+            putInt(DIALOG_KEY, dialogType)
+            putString("TITLE", title)
+        }
+        myDialog.arguments = bundle
+        myDialog.show(parentFragmentManager, "my_dialog")
     }
 
     override fun onSharedPreferenceChanged(sharedPreferences: SharedPreferences?, key: String?) {
