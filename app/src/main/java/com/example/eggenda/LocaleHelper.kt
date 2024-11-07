99988a56ac53e93a32e6fe2921e38650597ba6a1
Melody <153961300+mellli1231@users.noreply.github.com>
2024-11-07 15:11:49 -0800

    implement language settings
    - used locale and localehelper
    - add translation for chinese

---
+++ app/src/main/java/com/example/eggenda/LocaleHelper.kt
@@ -0,0 +1,74 @@
+package com.example.eggenda
+
+import android.annotation.TargetApi
+import android.content.Context
+import android.os.Build
+import androidx.preference.PreferenceManager
+import java.util.Locale
+
+object LocaleHelper {
+
+    private const val SELECTED_LANGUAGE = "Locale.Helper.Selected.Language"
+
+    fun onAttach(context: Context): Context {
+        println("inside onAttach 1")
+        val lang = getPersistedData(context, Locale.getDefault().getLanguage()).toString()
+        return setLocale(context, lang)
+    }
+
+    fun getLanguage(context: Context): String {
+        return getPersistedData(context, Locale.getDefault().language).toString()
+    }
+
+    fun setLocale(context: Context, language: String): Context {
+        persist(context, language)
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
+            return updateResources(context, language)
+        }
+
+        return updateResourcesLegacy(context, language)
+    }
+
+    fun getPersistedData(context: Context, defaultLanguage: String): String? {
+        val preferences = PreferenceManager.getDefaultSharedPreferences(context)
+        return preferences.getString(SELECTED_LANGUAGE, defaultLanguage)
+    }
+
+    private fun persist(context: Context, language: String) {
+        val preferences = PreferenceManager.getDefaultSharedPreferences(context)
+        val editor = preferences.edit()
+
+        editor.putString(SELECTED_LANGUAGE, language)
+        editor.apply()
+    }
+
+    @TargetApi(Build.VERSION_CODES.N)
+    private fun updateResources(context: Context, language: String): Context {
+        val locale = Locale(language)
+        Locale.setDefault(locale)
+
+        val configuration = context.resources.configuration
+        configuration.setLocale(locale)
+        configuration.setLayoutDirection(locale)
+
+        // Apply the new configuration
+        return context.createConfigurationContext(configuration)
+    }
+
+    @Suppress("DEPRECATION")
+    private fun updateResourcesLegacy(context: Context, language: String): Context {
+        val locale = Locale(language)
+        Locale.setDefault(locale)
+
+        val resources = context.resources
+        val configuration = resources.configuration
+        configuration.locale = locale
+        configuration.setLayoutDirection(locale)
+
+        // Update the configuration on older versions
+        resources.updateConfiguration(configuration, resources.displayMetrics)
+
+        return context
+    }
+}
--- app/src/main/java/com/example/eggenda/ui/settings/SettingsFragment.kt
+++ app/src/main/java/com/example/eggenda/ui/settings/SettingsFragment.kt
@@ -1,50 +1,29 @@
 package com.example.eggenda.ui.settings
 
-import android.app.AlertDialog
-import android.app.Dialog
-import android.content.DialogInterface
+
 import android.content.Intent
 import android.content.SharedPreferences
 import android.content.SharedPreferences.Editor
 import android.os.Bundle
-import android.view.View
-import android.widget.EditText
-import android.widget.RadioGroup
 import android.widget.Toast
-import androidx.fragment.app.DialogFragment
-import androidx.preference.CheckBoxPreference
 import androidx.preference.Preference
 import androidx.preference.PreferenceFragmentCompat
 import androidx.preference.PreferenceManager
+import com.example.eggenda.LocaleHelper
 import com.example.eggenda.R
-import com.example.eggenda.MainActivity
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.launch
 
-class SettingsFragment : PreferenceFragmentCompat(){
-    companion object {
-        const val METRIC = 0
-        const val IMPERIAL = 1
+class SettingsFragment : PreferenceFragmentCompat(), SharedPreferences.OnSharedPreferenceChangeListener{
 
-        const val DIALOG_KEY = "dialog"
-        const val STRING_DIALOG = 2
-        const val UNIT_DIALOG = 3
-    }
-
-    private var isAnonymous: Boolean = false
     private lateinit var sharedPreferences: SharedPreferences
     private lateinit var editor: Editor
 
     override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
         //load UI
-        addPreferencesFromResource(R.xml.preference)
+        setPreferencesFromResource(R.xml.preference, rootKey)
 
-        //initialize sharedPreferences and load settings
+        //initialize sharedPreference
         sharedPreferences = PreferenceManager.getDefaultSharedPreferences(requireContext())
         editor = sharedPreferences.edit()
-        loadPreferences()
-        println("saved settings loaded")
 
         //get user profile bar
         val profilePref: Preference? = findPreference("user_profile")
@@ -55,9 +34,36 @@ class SettingsFragment : PreferenceFragmentCompat(){
             startActivity(intent)
             true
         }
+
+        val language: Preference? = findPreference("language")
+        language?.setOnPreferenceChangeListener { preference, newValue ->
+            val languageCode = newValue as String
+            println("languageCode: $languageCode")
+            editor.putString("language", languageCode)
+            editor.apply()
+
+            if(LocaleHelper.getLanguage(requireContext()) != languageCode) {
+                println("changing language")
+                println("calling localeHelper from settings")
+                LocaleHelper.setLocale(requireContext(), languageCode)
+                Toast.makeText(requireActivity(), "Language changed", Toast.LENGTH_SHORT).show()
+                requireActivity().recreate()
+            }
+            true
+        }
     }
 
-    private fun loadPreferences() {
+    override fun onSharedPreferenceChanged(sharedPreferences: SharedPreferences?, key: String?) {
+        //needed for fragment to run
+    }
 
+    override fun onResume() {
+        super.onResume()
+        sharedPreferences.registerOnSharedPreferenceChangeListener(this)
+    }
+
+    override fun onPause() {
+        super.onPause()
+        sharedPreferences.unregisterOnSharedPreferenceChangeListener(this)
     }
 }
